06df9bd27661e88cb14c9ed049560fc94b91493e
Chris Wilcox <ckwilcox@gmail.com>
2013-03-31 22:59:03 -0700

    Continue work on data model for exercises

--- lift/models.py
+++ lift/models.py
@@ -1,53 +1,27 @@
 from django.db import models
+from django.db.models import Q
 from django.conf import settings
-
-
-
-
-class Weight(models.Model):
-    value = models.DecimalField()
-    unit = models.CharField(default="lbs")
-
-
-weights = [
-    1.25,1.25,
-    2.5,2.5,
-    5,5,
-    10,10,
-    25,25,
-    35,35,
-    45,45,45,45
-]
-
-barbells = [
-    40,
-]
-
 
 
 
 
 class Set(models.Model):
     weight = models.DecimalField()
-    try_reps = models.SmallInteger()
-    success_reps = models.SmallInteger()
+    reps_todo = models.SmallInteger()
+    reps_done = models.SmallInteger(default=0)
+    type = models.CharField(max_length=16)
 
+    @property
+    def failed(self):
+        return self.reps_done >= self.reps_todo:
 
-class ExerciseDef(models.Model):
-    name = models.CharField()
-    start_weight = models.DecimalField()
-    
-    def build_next_exercise(self, user):
-        exercises = Exercise.object.filter(definition=self, 
-                                            workout__user=user).order_by("-ts")
-        if exercises.count() > 0:
+    @property
+    def succeeded(self):
+        return not self.failed
 
-        return 
+class ExerciseData(models.Model):
 
-
-
-class Exercise(models.Model):
-
+    exercise_def
     ts = models.DateTimeField(auto_now_add=True, index_db=True)
 
     previous = models.ForeignKey('self', null=True, blank=True)
@@ -56,18 +30,64 @@ class Exercise(models.Model):
 
     sets = models.ManyToManyField(Set)
 
+    def did_fail_last_three(self):
+        if self.failed:
+            for exer in self.objects.filter(Q(next=self) & Q(next__next=self)):
+                if not exer.failed:
+                    return False
+            else:
+                return True
+        else:
+            return False
 
-class Workout(models.Model):
-    user = models.ForeignKey(settings.AUTH_USER_MODEL)
-    exercises = models.ManyToManyField(Exercise)
+    @property
+    def failed(self):
+        return any([x.failed in self.sets.all())
 
-    
+    @property
+    def succeeded(self):
+        return not self.failed
+
+
+class BarbellSquat():
+
+
+
+
+
+
+class ExerciseDef(models.Model):
+    display_name = models.CharField()
+
+
+    set_count = models.SmallInteger()
+
+    weight_start = models.DecimalField()
+    weight_increment = models.DecimalField()
+
+    video_url = models.CharField(max_length=256)
+
+
+    def build_first_exercise(self):
+
+
+
+    def build_next_exercise(self, user):
+        exercises = Exercise.object.filter(definition=self, 
+                                            workout__user=user).order_by("-ts")
+        if exercises.count() > 0:
+
+        
+
+
+
+
 class Routine(models.Model):
     name = models.CharField()  # Day A
-    exercises = models.ManyToManyField(ExerciseDef)
+    exercise_defs = models.ManyToManyField(ExerciseDef)
 
     def build_next_workout(self, user):
-        for exercise_def in self.exercises.all():
+        for exercise_def in self.exercise_defs.all():
             return Workout(user=user, exercises=exercise_def.build_next_exercise())
 
 
@@ -83,3 +103,18 @@ class Workout(models.Model):
     generated_from = models.ForeignKey(routine)
 
     exercise_defs = models.
+
+
+
+
+
+"What is today's workout?"
+- "answer is a list of exercises"
+
+"What weights should I use for this exercise?"
+- "answer is based on previous weights + available weights"
+
+
+
+
+
